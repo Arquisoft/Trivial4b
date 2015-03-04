620a07cf885404b78634f32074a446f5a3de8394
Sergio <sergio@asturiandev.com>
2015-03-04 21:34:40 +0100

    Añadido el módulo maven para el juego

---
+++ game/src/main/java/es/uniovi/asw/trivial/Main.java
@@ -0,0 +1,13 @@
+package es.uniovi.asw.trivial;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
